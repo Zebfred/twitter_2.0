04afc0b57786ce45baa4bb68abd17c5a09af15a4
Zebfred <zebfred22@gmail.com>
2019-12-19 08:35:44 -0800

    this could break

---
+++ twitoffDS9.60dc2ae8-a62d-486b-a72d-10563433e4f6/twitoffDS9/TWITOFF/app.py
@@ -0,0 +1,32 @@
+"""Code for ou app"""
+
+from decouple import config
+from flask import Flask, render_template, request
+from .models import DB, User
+
+#make our app factory
+
+def create_app():
+    app = Flask(__name__)
+
+    #add config for database
+    app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASE_URL')
+
+    #stop tracking modifications on sqlalchemy config
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    #have the database know about the app
+    DB.init_app(app)
+
+    @app.route('/')
+    def root():
+        users = User.query.all()
+        return render_template('base.html', title = 'Home', users=users)
+    
+    @app.route('/reset')
+    def reset():
+        DB.drop_all()
+        DB.create_all()
+        return render_template('base.html', title = 'Reset', users=[])
+    
+    return app
